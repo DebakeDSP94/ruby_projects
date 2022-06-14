cdce032e229d381dc2e19df00fc6e3a5a72fde77
stuart wilson <stuartwilson94@gmail.com>
2022-06-14 13:50:11 -0500

    fixed caesar_cipher to handle a negative shift

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,23 +2,50 @@
 def encode_msg(msg, shift)
   msg = msg.split('')
   msg_new = []
-  msg.each do |char|
-    ascii_code = char.ord
-    if (ascii_code >= 65 && ascii_code <= 90) 
-      ascii_code += shift % 26
-      if ascii_code > 90
-        ascii_code -= 26
-      end
-    elsif (ascii_code >= 97 && ascii_code <= 122)
-      ascii_code += shift % 26
-      if ascii_code > 122
-        ascii_code -= 26
-      end
-    end
-    msg_new << ascii_code.chr
+  if shift >= 0
+    pos_shift_encode(msg, shift, msg_new)
+  else
+    neg_shift_encode(msg, shift, msg_new)
   end
   msg_out = msg_new.join('')
   $stdout.puts("The encoded message is: #{msg_out}")
+end
+
+def pos_shift_encode(msg, shift, msg_new)
+  msg.each do |char|
+    ascii_code = char.ord
+      if (ascii_code >= 65 && ascii_code <= 90) 
+        ascii_code += shift % 26
+        if ascii_code > 90
+          ascii_code -= 26
+        end
+      elsif (ascii_code >= 97 && ascii_code <= 122)
+        ascii_code += shift % 26
+        if ascii_code > 122
+          ascii_code -= 26
+        end
+        msg_new << ascii_code.chr
+      end
+  end
+end
+
+def neg_shift_encode(msg, shift, msg_new)
+  shift = shift.abs
+  msg.each do |char|
+    ascii_code = char.ord
+      if (ascii_code >= 65 && ascii_code <= 90) 
+        ascii_code -= shift % 26
+        if ascii_code < 65
+          ascii_code += 26
+        end
+      elsif (ascii_code >= 97 && ascii_code <= 122)
+        ascii_code -= shift % 26 
+        if ascii_code < 97
+           ascii_code += 26
+        end
+      end
+    msg_new << ascii_code.chr
+  end
 end
 
 msg = "hello World!"
@@ -27,6 +54,6 @@ shift = 12
 encode_msg(msg, shift)
 
 msg = "Wow, what a great string!"
-shift = -12
 
-encode_msg(msg, shift)
+
+encode_msg(msg, -27)
